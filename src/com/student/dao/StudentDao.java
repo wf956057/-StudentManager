a4b04634d6f6ff03ef1cf6ef953b0f1d6b44231d
Administrator <Administrator@192.168.3.10>
2018-06-17 13:59:42 +0900

    王鋒

--- src/com/student/dao/StudentDao.java
+++ src/com/student/dao/StudentDao.java
@@ -7,13 +7,10 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.student.bean.Student;
 import com.student.mapper.StudentMapper;
+import org.apache.log4j.*;
+import org.apache.log4j.xml.*;
+import java.io.*;
 
-/**
- * StudentDao クラス Sqlの実行クラス
- *
- * @author jiangdang
- *
- */
 public class StudentDao {
 
 	/**
@@ -25,7 +22,7 @@ public class StudentDao {
 	 * spring提供的类
 	 *
 	 * @param jdbcTemplate
-	 *            返回值类型： void
+	 * 返回值类型： void
 	 */
 	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
 		this.jdbcTemplate = jdbcTemplate;
@@ -33,46 +30,52 @@ public class StudentDao {
 
 	/**
 	 * 查询所有学生
-	 *
 	 * @return 返回值类型： List<Student>
 	 */
 	public List<Student> queryAll() {
-		String sql = "select id,name,birthday,age,score,telephone,poslnumber,address, classid from student";
-		// 将查询结果映射到Student类中，添加到list中，并返回
+
+		 //(1)Loggerオブジェクトの生成
+	 //   Logger log = Logger.getLogger(ExLog1.class.getName());
+	    //(2)設定ファイルの読み込み
+//        DOMConfigurator.configure("log4j.xml");
+//        ExLog1 obj1 = new ExLog1();
+//        obj1.methodA();
+
+
+
+
+		String sql = "select id,name,birthday,age,score from student";
+		//将查询结果映射到Student类中，添加到list中，并返回
 		return jdbcTemplate.query(sql, new StudentMapper());
 	}
 
 	/**
 	 * 通过姓名查询
-	 *
 	 * @param name
 	 * @return 返回值类型： List<Student>
 	 */
 	public List<Student> queryByName(String name) {
-		String sql = "select id,name,birthday,age,score,telephone,poslnumber,address, classid from student where name like '%"
-				+ name + "%'";
+		String sql = "select id,name,birthday,age,score  from student where name like '%" + name + "%'";
 
 		return jdbcTemplate.query(sql, new StudentMapper());
 	}
 
 	/**
 	 * 添加学生
-	 *
 	 * @param student
 	 * @return 返回值类型： boolean
 	 */
 	public boolean addStu(Student student) {
-		String sql = "insert into student(id,name,birthday,age,score,telephone,poslnumber,address,classid) values(0,?,?,?,?,?,?,?,?)";
+		String sql = "insert into student(id,name,birthday,age,score) values(0,?,?,?,?)";
 
 		return jdbcTemplate.update(sql,
-				new Object[] { student.getName(), student.getBirthday(), student.getAge(), student.getScore(),
-						student.getTelephone(), student.getPoslnumber(), student.getAddress(), student.getClassid() },
-				new int[] { Types.VARCHAR, Types.VARCHAR, Types.INTEGER, Types.DOUBLE, Types.VARCHAR, Types.VARCHAR, Types.VARCHAR, Types.INTEGER}) == 1;
+				new Object[] { student.getName(), student.getBirthday(), student.getAge(),
+						student.getScore() },
+				new int[] { Types.VARCHAR, Types.VARCHAR, Types.INTEGER, Types.DOUBLE }) == 1;
 	}
 
 	/**
 	 * 删除学生
-	 *
 	 * @param id
 	 * @return 返回值类型： boolean
 	 */
@@ -84,15 +87,14 @@ public class StudentDao {
 
 	/**
 	 * 更新学生信息
-	 *
 	 * @param student
 	 * @return 返回值类型： boolean
 	 */
 	public boolean updateStu(Student student) {
 
-		String sql = "update student set name=? ,age=?,birthday = ? ,score = ?,telephone = ?,poslnumber = ? ,address = ?, classid = ?  where id = ?";
-		Object stuObj[] = new Object[] { student.getName(), student.getAge(), student.getBirthday(), student.getScore(),
-				student.getTelephone(), student.getPoslnumber(), student.getId(), student.getAddress(), student.getClassid() };
+		String sql = "update student set name=? ,age=?,birthday = ? ,score = ? where id = ?";
+		Object stuObj[] = new Object[] {student.getName(), student.getAge(), student.getBirthday(),
+				student.getScore(), student.getId() };
 
 		return jdbcTemplate.update(sql, stuObj) == 1;
 	}
